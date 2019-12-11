3484afa94cd2b64b34efbc3dab01e0b808ed8cf9
nanshens <619232906@qq.com>
2019-12-11 17:35:29 +0800

    new version start

---
+++ src/main/java/ns/boot/jpa/starter/JpaQueryFactory.java
@@ -0,0 +1,67 @@
+package ns.boot.jpa.starter;
+
+/**
+ * @author ns
+ */
+
+public class JpaQueryFactory {
+	public static void main(String[] args) {
+		/* create
+		* need many factories to support different input
+		* need generic<T> to limit result type
+		*
+		* */
+		JpaQueryFactory factory = new JpaQueryFactory();
+
+		/* input
+		* 1. factory.input(json)--------自定义json结构体
+		* 2. factory.input(jpaquery)----自定义jpaquery
+		* 3. factory.input(sql)---------自定义sql
+		*
+		* */
+
+		/*
+		* cache
+		*
+		* user custom cache method
+		*
+		* factory.cache()
+		*
+		*
+		* */
+
+		/* parser implicit
+		*
+		*
+		*
+		*
+		* */
+
+		/*
+		 * cache implicit
+		 *
+		 * default cache method
+		 *
+		 * */
+
+		/*
+		* query implicit
+		*
+		*
+		*
+		*
+		* */
+
+		/*
+		* result
+		* list? page? json?
+		*
+		* factory.resultList();
+		* factory.resultPage();
+		* factory.resultJson();
+		*
+		*
+		* */
+
+	}
+}
