3373d61652423003c1211f4d5de0f56287155906
nanshens <619232906@qq.com>
2019-12-11 23:11:18 +0800

    init

--- src/main/java/ns/boot/jpa/starter/JpaQuery.java
+++ src/main/java/ns/boot/jpa/starter/JpaQuery.java
@@ -1,10 +1,50 @@
 package ns.boot.jpa.starter;
 
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 /**
  * @author ns
  */
 
 public class JpaQuery {
 
+	public void input(JSONObject query) {
 
+	}
+
+	public void input(String query) {
+
+	}
+
+	public void input(JpaQueryT query) {
+
+	}
+
+	public void cache() {
+
+	}
+
+	private void parser() {
+
+	}
+
+
+	private void query() {
+
+	}
+
+	private JSONObject resultJson() {
+		return null;
+	}
+
+	private List resultList() {
+		return null;
+	}
+
+	private Page resultPage() {
+		return null;
+	}
 }
