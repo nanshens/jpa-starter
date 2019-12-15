d3005d5d132f79a87d328d69c6854fa3f2320e2b
nanshens <619232906@qq.com>
2019-12-15 22:35:52 +0800

    sqlquery update

--- src/main/java/ns/boot/jpa/starter/JpaCustomQuery.java
+++ src/main/java/ns/boot/jpa/starter/JpaCustomQuery.java
@@ -1,13 +1,42 @@
 package ns.boot.jpa.starter;
 
+import com.alibaba.fastjson.JSON;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
 /**
  * @author ns
  */
 
-public class JpaCustomQuery extends JpaQuery{
+public class JpaCustomQuery<T> extends JpaQuery<T>{
+	private CustomQuery<T> customQuery;
 
-	public void input(CustomQuery query) {
-		super.customQuery = query;
+	public JpaCustomQuery(Class<T> tClass) {
+		super(tClass);
 	}
 
+	public JpaCustomQuery<T> input(CustomQuery<T> customQuery){
+		this.customQuery = customQuery;
+		return this;
+	}
+
+	private void buildSpecification(){
+
+	}
+
+	@Override
+	public JSON resultJson() {
+		return null;
+	}
+
+	@Override
+	public List<T> resultList() {
+		return null;
+	}
+
+	@Override
+	public Page<T> resultPage() {
+		return null;
+	}
 }
