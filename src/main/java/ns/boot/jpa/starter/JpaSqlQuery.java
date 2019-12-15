913e71edc7e12a3aa75d2e1aaf7bdd0276f8398a
nanshens <619232906@qq.com>
2019-12-15 22:26:31 +0800

    jsonquery update

--- src/main/java/ns/boot/jpa/starter/JpaSqlQuery.java
+++ src/main/java/ns/boot/jpa/starter/JpaSqlQuery.java
@@ -1,12 +1,57 @@
 package ns.boot.jpa.starter;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import javax.persistence.Query;
+import java.util.List;
+
 /**
  * @author ns
  */
 
-public class JpaSqlQuery extends JpaQuery{
+public class JpaSqlQuery<T> extends JpaQuery<T>{
+	private String sqlQuery;
 
-	public void input(String query) {
-		super.sqlQuery = query;
+	public JpaSqlQuery(Class<T> tClass) {
+		super(tClass);
+	}
+
+	public JpaSqlQuery<T> input(String sqlQuery) {
+		this.sqlQuery = sqlQuery;
+		return this;
+	}
+
+	public List<T> cache() {
+		return null;
+	}
+
+	public void parser() {
+	}
+
+	private List<T> query() {
+		Query query = getEm().createNativeQuery(sqlQuery, entityClass);
+		return query.getResultList();
+	}
+
+	@Override
+	public JSON resultJson() {
+		return buildJson(query());
+	}
+
+	private JSONArray buildJson(List result) {
+		return new JSONArray(result);
+	}
+
+	@Override
+	public List<T> resultList() {
+		return query();
+	}
+
+	@Override
+	public Page<T> resultPage() {
+		return new PageImpl<>(query());
 	}
 }
